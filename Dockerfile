56df1f6324482319ae957a8d481055c4f6efc25e
Stephane-ISEN <stephane.jamin-normand@isen-ouest.yncrea.fr>
2024-02-02 14:49:43 +0100

    Correction Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ COPY requirements.txt /home/app/
 RUN pip install -r requirements.txt
 COPY . /home/app
 CMD mlflow server --host 0.0.0.0 -p $PORT \
- -- default-artifact-root $ARTIFACT_STORE_URI \
- -- backend-store-uri $BACKEND_STORE_URI
+ --default-artifact-root $ARTIFACT_STORE_URI \
+ --backend-store-uri $BACKEND_STORE_URI
--- push.sh
+++ push.sh
@@ -1,13 +1,14 @@
-# Construction de l'image docker et push sur Heroku
+# connection au service de conteneurs
+heroku container:login
 
 # Construction de l'image docker et push sur Heroku
 docker build . -t streamlit_isen
 
 # Tag de l'image au registre Heroku
-docker tag streamlit_isen registry.heroku.com/streamlit-isen-stephane/web
+docker tag streamlit_isen registry.heroku.com/isen-mlflow-stephane/web
 
 # Push de l'image sur Heroku
-docker push registry.heroku.com/streamlit-isen-stephane/web              
+docker push registry.heroku.com/isen-mlflow-stephane/web              
 
 # Lancement de l'application
-heroku container:release web -a streamlit-isen-stephane                  
+heroku container:release web -a isen-mlflow-stephane     
